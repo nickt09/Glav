80d260bbf1d900a38ab570ef533e917c9657b16f
nickt09 <fullsky82@protonmail.com>
2022-11-11 11:03:35 +0300

    hm3

---
+++ trenirovka/js/main.js
@@ -0,0 +1,61 @@
+// "use strict"
+//
+// let x = "life";
+// let c = "have";
+//
+// console.log(x + " " + c);
+//
+// console.log(`${x} privet ${c}`);
+//
+// let m = 200 > 300;
+//
+// console.log(m);
+//
+//
+// let v = true;
+// console.log(typeof v);
+//
+//
+// let num = 645363
+// console.log(typeof (num + " "));   конвертация числа в строку
+
+
+// let x = "3267846";   конвертация строки в число
+// console.log(+ x);
+
+
+// console.log(!100);  булевые значения. будет false
+// console.log(!!"");  булевые значения. будет false
+// console.log(!!1);   булевые значения. будет true, потому что true, это 1
+
+
+// let user = {
+//     id: 1,
+//     name: "Nikola",
+//     age: 23,
+//     status: true,
+// };
+// console.log(user);       Это объект
+
+
+// let arr = [1, 2, 3, 4, 5,];
+// console.log(arr);           Это объект
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

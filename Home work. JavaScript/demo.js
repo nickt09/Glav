4b85816d3ced81950d8259bfaa5728329647e6d5
nickt09 <fullsky82@protonmail.com>
2022-11-11 11:21:06 +0300

    hm4

---
+++ Home work. JavaScript/demo.js
@@ -0,0 +1,46 @@
+let a = "hello";
+let b = "owu";
+let c = "com";
+let d = "ua";
+let e = 1;
+let f = 10;
+let g = -999;
+let h = 123;
+let i = 3.14;
+let j = 2.7;
+let k = 16;
+let l = true;
+let m = false;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+console.log(h);
+console.log(i);
+console.log(j);
+console.log(k);
+console.log(l);
+console.log(m);
+
+
+
+let firstName = "Хаманов";
+let middleName = "Иван";
+let lastName = "Кармелович";
+
+let person = firstName + " " + middleName + " " + lastName;
+console.log(person);
+
+
+let aa = 100;
+let bb = "100";
+let cc = true;
+console.log(typeof aa);
+console.log(typeof bb);
+console.log(typeof cc);
+
+
